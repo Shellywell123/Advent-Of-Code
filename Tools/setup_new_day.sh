acfd55f3c835f273cb3209d03041f6871424478c
Shellywell123 <benjamin@shellswell.com>
2022-01-09 18:11:30 +0000

    script update

--- Tools/setup_new_day.sh
+++ Tools/setup_new_day.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 # small script to help with the setup of each AOC puzzle
 
+# user input year
+
+echo "What year are you working on: (input year e.g 2021)"
+read y
+
 # user input day num x/25
-echo "What number day do you want to setup: (recommend 0X format if less than 10)"
+echo "What number day do you want to setup: (input no e.g 5)"
 read x
 
+url=https://adventofcode.com/${y}/day/${x}/input
+
+#if one char add 0 to front to maintain file order
+len=`expr length "${x}"`
+if [[ ${len}=1 ]];
+then 
+num="0${x}"
+else
+    num=${x}
+fi
+
 # user input file ext 
-echo "What Language will you be using: (input file ext) python=py C++=cpp: "
+echo "What Language will you be using: (input file ext e.g python=py C++=cpp)"
 read ext
 
 # create dir
-mkdir Day${x}
+mkdir Day${num}
 
-#create files
-touch Day${x}/Part_1.${ext} Day${x}/Part_2.${ext} Day${x}/inputs.txt Day${x}/tests.txt
+# create files
+touch Day${num}/Part_1.${ext} Day${num}/Part_2.${ext} Day${num}/inputs.txt Day${num}/tests.txt
+
+# populate inputs.txt (currently requires login)
+curl -o Day${num}/inputs.txt ${url}
