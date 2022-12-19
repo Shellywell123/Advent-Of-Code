ff904671aeb4e20c6e56feddea20961c52fa4f8c
Shellywell123 <benjamin@shellswell.com>
2022-12-19 18:32:49 +0000

    AOC 2022 Day16 Python

--- AdventOfCode2022/Python/Day16/Day16.py
+++ AdventOfCode2022/Python/Day16/Day16.py
@@ -1,4 +1,4 @@
-import numpy as np
+import networkx as nx
 
 def ParseData(file):    
     """
@@ -15,75 +15,109 @@ def ParseData(file):
         rate    = int((line_string.split('=')[1]).split(';')[0])
         tunnels = (line_string[line_string.index('to valve')+9:]).strip().split(', ')
         valves[name] = {"Rate": rate, "Tunnels": tunnels}
+    
     return valves
 
 def Part1(filename, printing = False):
     """
     Solutions to Part 1
     """
-    valves = ParseData(filename)
-
-    # for valve in valves:
-    print(valves)
-    
-    open_valves = []
-    current_valve = 'AA'
-    released = 0
-    for minute in range(1,30 +1):
-
-        moving = False
-        opening = False
-
-        if valves[current_valve]["Rate"] == 0:
-            # dont add to open valves
-            moving = True
-        else:
-            opening = True
-
-        if moving:
-            nvr = 0
-            for nv in valves[current_valve]["Tunnels"]:
-                if valves[nv]["Rate"] > nvr:
-                    nvr = valves[nv]["Rate"]
-                    next_valve = nv
-
-        if opening:
-            pass
-
-        pressure = 0
-        if printing:
-            print(f'== Minute {minute} == (at valve {current_valve})')
-            print(f'open valves = {open_valves}, releasing {pressure} pressure')
-            if opening:
-                print(f'You open valve {current_valve}\n')
-            if moving:
-                print(f'You move to valve {next_valve}\n')
-        if moving:
-            current_valve = next_valve
-        released += pressure   
-
-    
-    return 0
-
-def Matrix(filename):
-    """
-    """
 
     valves = ParseData(filename)
 
-    distances = {}
+    Temp = nx.Graph()
+    edges = []
+    for valve1 in valves:
+        Temp.add_node(valve1)
+        for valve2 in dict(valves[valve1])["Tunnels"]:
+            edges.append((valve1,valve2, {"weight": 1}))
+    Temp.add_edges_from(edges)
+
+    distances = dict(nx.all_pairs_shortest_path_length(Temp))
+
+    non_zero_valves = []
     for valve in valves:
-        print(valve)
+        if dict(valves[valve])["Rate"] != 0:
+            non_zero_valves.append(valve)
 
+    open_valves = []
 
-    return 0
+    def decide(current_valve, open_valves, minutes):
+        """
+        """
+        if minutes < 0:
+            return 0
+        
+        current_best_decision_result = 0
+        # think about moving to other valves
+        for next_valve in non_zero_valves:
+            if current_valve == next_valve: 
+                continue
+            dist = distances[current_valve][next_valve]
+            rate = dict(valves[next_valve])["Rate"]
+            # try and moving to a valve and opening it
+            if next_valve not in open_valves and dist < minutes:
+                open_valves.append(next_valve)
+                minutes_if_open_valve = minutes - (dist + 1)
+                relief = rate*minutes_if_open_valve
+                open_decision_result, open_valves = decide(next_valve,open_valves, minutes_if_open_valve)
+                open_decision_result = open_decision_result + relief
+                if open_decision_result >= current_best_decision_result:
+                    current_best_decision_result = open_decision_result
+                # if it wasnt worth opening close it and try the next valve
+                open_valves.remove(next_valve)
+
+        return current_best_decision_result, open_valves
+
+    return decide("AA",[],30)[0]
 
 def Part2(filename, printing = False):
     """
     Solutions to Part 2
     """
-     
-    return 0
+
+    
+    valves = ParseData(filename)
+
+    Temp = nx.Graph()
+    edges = []
+    for valve1 in valves:
+        Temp.add_node(valve1)
+        for valve2 in dict(valves[valve1])["Tunnels"]:
+            edges.append((valve1,valve2, {"weight": 1}))
+    Temp.add_edges_from(edges)
+
+    distances = dict(nx.all_pairs_shortest_path_length(Temp))
+
+    non_zero_valves = []
+    for valve in valves:
+        if dict(valves[valve])["Rate"] != 0:
+            non_zero_valves.append(valve)
+
+    def decide(current_valve, open_valves, minutes):
+        """
+        """
+        if minutes < 0:
+            return 0
+        
+        current_best_decision_result = 0
+        # think about moving to other valves
+        for next_valve in non_zero_valves:
+            if current_valve == next_valve or next_valve in open_valves or distances[current_valve][next_valve] > minutes:
+                continue
+            open_valves.append(next_valve)
+            minutes_if_open_valve = minutes - (distances[current_valve][next_valve] + 1)
+            relief = dict(valves[next_valve])["Rate"]*minutes_if_open_valve
+            open_decision_result, open_valves = decide(next_valve, open_valves, minutes_if_open_valve)
+            open_decision_result = open_decision_result + relief
+            if open_decision_result >= current_best_decision_result:
+                current_best_decision_result = open_decision_result
+            # if it wasnt worth opening close it and try the next valve
+            open_valves.remove(next_valve)
+
+        return current_best_decision_result, open_valves
+
+    return 'TODO'##decide("AA",[],26)[0]
 
 def main():
     """
@@ -94,14 +128,14 @@ def main():
 
     print("Advent-Of-Code 2022 - Day16")
 
-    #print(f'"{testfile}": Answer to Part 1 = {Part1(testfile, printing = True)}')    
-    #print(f'"{testfile}": Answer to Part 2 = {Part2(testfile, 20, printing = False)}\n')
-    Matrix(testfile)
-    #assert(Part1(testfile) == 1651)
+    print(f'"{testfile}": Answer to Part 1 = {Part1(testfile, printing = True)}')    
+    print(f'"{testfile}": Answer to Part 2 = {Part2(testfile, printing = False)}\n')
+
+    assert(Part1(testfile) == 1651)
     #assert(Part2(testfile,20) == 56000011)
 
-    #print(f'"{inputfile}": Answer to Part 1 = {Part1(inputfile, 2000000, printing = False)}')
-    #print(f'"{inputfile}": Answer to Part 2 = {Part2(inputfile, 4000000, printing = False)}')
+    print(f'"{inputfile}": Answer to Part 1 = {Part1(inputfile, printing = False)}')
+    print(f'"{inputfile}": Answer to Part 2 = {Part2(inputfile, printing = False)}')
 
 if __name__ == "__main__":
     main()
