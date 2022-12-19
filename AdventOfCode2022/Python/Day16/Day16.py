e8fa2818bcb13fb0568143b6ba52b9fd23665ef5
shellywell123 <benjamin@shellswell.com>
2022-12-19 21:32:54 +0000

    AOC 2022 Day16 Python

--- AdventOfCode2022/Python/Day16/Day16.py
+++ AdventOfCode2022/Python/Day16/Day16.py
@@ -3,7 +3,6 @@ import networkx as nx
 def ParseData(file):    
     """
     function to read and parse in data from a txt file
-    (rather messy)
     """
     with open(file) as f:
         data = f.read().split('\n')
@@ -21,6 +20,8 @@ def ParseData(file):
 def Part1(filename, printing = False):
     """
     Solutions to Part 1
+    First time encountering dymnamic programming
+    TOP DOWN recursion with caching
     """
 
     valves = ParseData(filename)
@@ -49,19 +50,23 @@ def Part1(filename, printing = False):
             return 0
         
         current_best_decision_result = 0
-        # think about moving to other valves
+        # try moving to a valve and opening it
         for next_valve in non_zero_valves:
+
             if current_valve == next_valve: 
                 continue
+
             dist = distances[current_valve][next_valve]
             rate = dict(valves[next_valve])["Rate"]
-            # try and moving to a valve and opening it
+
             if next_valve not in open_valves and dist < minutes:
                 open_valves.append(next_valve)
                 minutes_if_open_valve = minutes - (dist + 1)
                 relief = rate*minutes_if_open_valve
                 open_decision_result, open_valves = decide(next_valve,open_valves, minutes_if_open_valve)
                 open_decision_result = open_decision_result + relief
+
+                # check if result was highest result yet
                 if open_decision_result >= current_best_decision_result:
                     current_best_decision_result = open_decision_result
                 # if it wasnt worth opening close it and try the next valve
@@ -75,8 +80,7 @@ def Part2(filename, printing = False):
     """
     Solutions to Part 2
     """
-
-    
+   
     valves = ParseData(filename)
 
     Temp = nx.Graph()
@@ -94,30 +98,69 @@ def Part2(filename, printing = False):
         if dict(valves[valve])["Rate"] != 0:
             non_zero_valves.append(valve)
 
-    def decide(current_valve, open_valves, minutes):
+    def decide(current_valve, ele_current_valve, open_valves, minutes, ele_minutes):
         """
         """
         if minutes < 0:
             return 0
         
         current_best_decision_result = 0
-        # think about moving to other valves
+        # try moving to a valve and opening it
         for next_valve in non_zero_valves:
-            if current_valve == next_valve or next_valve in open_valves or distances[current_valve][next_valve] > minutes:
+
+            if current_valve == next_valve: 
                 continue
-            open_valves.append(next_valve)
-            minutes_if_open_valve = minutes - (distances[current_valve][next_valve] + 1)
-            relief = dict(valves[next_valve])["Rate"]*minutes_if_open_valve
-            open_decision_result, open_valves = decide(next_valve, open_valves, minutes_if_open_valve)
-            open_decision_result = open_decision_result + relief
-            if open_decision_result >= current_best_decision_result:
-                current_best_decision_result = open_decision_result
-            # if it wasnt worth opening close it and try the next valve
-            open_valves.remove(next_valve)
+
+            dist = distances[current_valve][next_valve]
+            rate = dict(valves[next_valve])["Rate"]
+
+            if next_valve not in open_valves and dist < minutes:
+                open_valves.append(next_valve)
+                minutes_if_open_valve = minutes - (dist + 1)
+                relief = rate*minutes_if_open_valve
+                open_decision_result, open_valves = ele_decide(next_valve, ele_current_valve, open_valves, minutes_if_open_valve, ele_minutes)
+                open_decision_result = open_decision_result + relief
+
+                # check if result was highest result yet
+                if open_decision_result >= current_best_decision_result:
+                    current_best_decision_result = open_decision_result
+                # if it wasnt worth opening close it and try the next valve
+                open_valves.remove(next_valve)
+
+        return current_best_decision_result, open_valves
+    
+    def ele_decide(current_valve, ele_current_valve, open_valves, minutes, ele_minutes):
+        """
+        """
+        if minutes < 0:
+            return 0
+        
+        current_best_decision_result = 0
+        # try moving to a valve and opening it
+        for ele_next_valve in non_zero_valves:
+
+            if ele_current_valve == ele_next_valve: 
+                continue
+
+            dist = distances[ele_current_valve][ele_next_valve]
+            rate = dict(valves[ele_next_valve])["Rate"]
+
+            if ele_next_valve not in open_valves and dist < ele_minutes:
+                open_valves.append(ele_next_valve)
+                ele_minutes_if_open_valve = ele_minutes - (dist + 1)
+                relief = rate*ele_minutes_if_open_valve
+                open_decision_result, open_valves = decide(current_valve, ele_next_valve, open_valves, minutes, ele_minutes_if_open_valve)
+                open_decision_result = open_decision_result + relief
+
+                # check if result was highest result yet
+                if open_decision_result >= current_best_decision_result:
+                    current_best_decision_result = open_decision_result
+                # if it wasnt worth opening close it and try the next valve
+                open_valves.remove(ele_next_valve)
 
         return current_best_decision_result, open_valves
 
-    return 'TODO'##decide("AA",[],26)[0]
+    return decide("AA","AA",[],26, 26)[0]
 
 def main():
     """
@@ -128,14 +171,14 @@ def main():
 
     print("Advent-Of-Code 2022 - Day16")
 
-    print(f'"{testfile}": Answer to Part 1 = {Part1(testfile, printing = True)}')    
-    print(f'"{testfile}": Answer to Part 2 = {Part2(testfile, printing = False)}\n')
+    print(f'Tests : Answer to Part 1 = {Part1(testfile, printing = False)}')    
+    print(f'Tests : Answer to Part 2 = {Part2(testfile, printing = False)}\n')
 
     assert(Part1(testfile) == 1651)
-    #assert(Part2(testfile,20) == 56000011)
+    assert(Part2(testfile) == 1707)
 
-    print(f'"{inputfile}": Answer to Part 1 = {Part1(inputfile, printing = False)}')
-    print(f'"{inputfile}": Answer to Part 2 = {Part2(inputfile, printing = False)}')
+    print(f'Inputs: Answer to Part 1 = {Part1(inputfile, printing = False)}')
+    print(f'Inputs: Answer to Part 2 = {Part2(inputfile, printing = False)}')
 
 if __name__ == "__main__":
     main()
