e0030eb93532de4a355b0151ef56e55806414e86
Shellywell123 <benjamin@shellswell.com>
2022-12-28 16:02:21 +0000

    AOC 2022 Day23 Python

--- AdventOfCode2022/Python/Day23/Day23.py
+++ AdventOfCode2022/Python/Day23/Day23.py
@@ -16,95 +16,208 @@ def ParseData(file):
                 elf_no += 1
 
     return elves
-    
+
 def Part1(filename, printing = False):
     """
     Solutions to Part 1
     """
     elves = ParseData(filename)
 
-    for r in range(0,3):
-        print()
+    for r in range(0,10):
+        if printing:            
+            print()
+        # propose next postions
         for e in range(0,len(elves)):
 
             elf = dict(elves[e])
 
             [x,y] = elf['pos']
 
-            N  = [x  ,y-1]
-            NE = [x+1,y-1]
-            NW = [x-1,y-1]
-            S  = [x  ,y+1]
-            SE = [x+1,y+1]
-            SW = [x-1,y+1]
-            E  = [x+1,y  ]
-            W  = [x-1,y  ]
+            N_set  = [[x  ,y-1],[x+1,y-1],[x-1,y-1]]
+            S_set  = [[x  ,y+1],[x+1,y+1],[x-1,y+1]]
+            E_set  = [[x+1,y  ],[x+1,y-1],[x+1,y+1]]
+            W_set  = [[x-1,y  ],[x-1,y-1],[x-1,y+1]]
 
-            next_posss = [[N,NE,NW],[S,SE,SW],[W,SW,NW],[E,SE,NE]]
-            #next_posss = next_posss[(elf['last_dir']+1)%len(next_posss):] + next_posss[:(elf['last_dir']+1)%len(next_posss)]
-            #print(next_posss)
+            next_pos_sets = [N_set,S_set,W_set,E_set]
             
-            # make all decisions 
-            set = False
-            for d in range(elf['last_dir']+1,elf['last_dir']+5):
-                next_poss = next_posss[d%4]
-                for next_pos in next_poss:
-                    if not set:                        
-                        if next_pos not in [elf['pos'] for elf in elves]:
-                            elf['next_pos'] = next_poss[0]
-                            elf['next_pos'] = elf['pos']
+            proposed = False
+            while not proposed:
+                current_elf_positions =  [elf['pos'] for elf in elves]  
+
+                # if no other elf nearbye do nothing
+                if not any((epos in current_elf_positions) for epos in N_set+S_set+E_set+W_set):
+                    elf['next_pos'] = elf['pos']
+                    elves[e] = elf
+                    proposed = True
+                    break
+
+                # check if surrounded at all sides:
+                elif all(any((epos in current_elf_positions) for epos in set) for set in next_pos_sets):
+                    elf['next_pos'] = elf['pos']
+                    elves[e] = elf
+                    proposed = True
+                    break
+
+                # propse
+                else:  
+                    for d in range(0,4):
+                        next_pos_set = next_pos_sets[(d+r)%4]  
+                        if ((next_pos_set[0] not in current_elf_positions) and
+                        (next_pos_set[1] not in current_elf_positions) and
+                        (next_pos_set[2] not in current_elf_positions)):  
+                            elf['next_pos'] = next_pos_set[0]
                             elf['last_dir'] = d
                             elves[e] = elf
-                            set = True
-                    
-            print(r,elf,list('nswe')[elf['last_dir']%4])
+                            proposed = True
+                            break
+            if printing:
+                print(r+1,elf,list('nswe')[elf['last_dir']%4])
+            
+        # evaluate proposed positions
+        proposed_elf_positions =  [elf['next_pos'] for elf in elves]
 
-
-        
         for e in range(0,len(elves)):
             elf = dict(elves[e])
-            if next_pos not in [elf2['pos'] for elf2 in elves if elf2['elf_no'] != elf['elf_no']]:
-                elf['pos'] = elf['next_pos']
-                elves[e] = elf
+            # dups
+            if proposed_elf_positions.count(elf['next_pos']) > 1:
+                elf['next_pos'] = elf['pos']
+            else:
+                elf['pos'] = elf['next_pos']            
+            elves[e] = elf
 
         xs = []
         ys = []
-        print()
+        
         for e in range(0,len(elves)):
             elf = dict(elves[e])
             xs.append(elf['pos'][0])
             ys.append(elf['pos'][1])
-        
-        grid = []
-        for y in range(-max(ys),max(ys)+1):
-            row = []
-            for x in range(-max(xs),max(xs)+1):
-                row.append('.')
-            grid.append(row)
-        
-        for row in grid:
-            print(row)
-        
-        for elf in elves:
-            [x,y] = dict(elf)['pos']
-            grid[y][x] = '#'
-        
-        for row in grid:
-            print(row)
-        
 
-        
-    ans = (max(xs) - min(xs)) * ((max(ys) - min(ys))) - len(elves)
+        if printing:   
+            print()                   
+            grid = []
+            for y in range(-max(ys),max(ys)+1):
+                row = []
+                for x in range(-max(xs),max(xs)+1):
+                    row.append('.')
+                grid.append(row)
             
-    return ans
+            for elf in elves:
+                [x,y] = dict(elf)['pos']
+                grid[y][x] = '#'#str(elf['elf_no'])
+            
+            grid[0][0] = '0'
+            for row in grid:
+                rowstr = ''
+                for r in row:
+                    rowstr += r
+                print(rowstr)
+    
+    return (max(xs) - min(xs)+1) * ((max(ys) - min(ys)+1)) - len(elves)
 
 def Part2(filename, printing = False):
     """
     Solutions to Part 2
     """
-    encrypted_file = ParseData(filename)
+    elves = ParseData(filename)
 
-    return 0
+    stable = 0
+    r = 0
+    while stable < len(elves):
+        stable = 0
+        if printing:            
+            print()
+        # propose next postions
+        for e in range(0,len(elves)):
+
+            elf = dict(elves[e])
+
+            [x,y] = elf['pos']
+
+            N_set  = [[x  ,y-1],[x+1,y-1],[x-1,y-1]]
+            S_set  = [[x  ,y+1],[x+1,y+1],[x-1,y+1]]
+            E_set  = [[x+1,y  ],[x+1,y-1],[x+1,y+1]]
+            W_set  = [[x-1,y  ],[x-1,y-1],[x-1,y+1]]
+
+            next_pos_sets = [N_set,S_set,W_set,E_set]
+            
+            proposed = False
+            while not proposed:
+                current_elf_positions =  [elf['pos'] for elf in elves]  
+
+                # if no other elf nearbye do nothing
+                if not any((epos in current_elf_positions) for epos in N_set+S_set+E_set+W_set):
+                    elf['next_pos'] = elf['pos']
+                    elves[e] = elf
+                    proposed = True
+                    stable += 1 
+                    break
+
+                # check if surrounded at all sides:
+                elif all(any((epos in current_elf_positions) for epos in set) for set in next_pos_sets):
+                    elf['next_pos'] = elf['pos']
+                    elves[e] = elf
+                    proposed = True
+                    stable += 1 
+                    break
+
+                # propse
+                else:  
+                    for d in range(0,4):
+                        next_pos_set = next_pos_sets[(d+r)%4]  
+                        if ((next_pos_set[0] not in current_elf_positions) and
+                        (next_pos_set[1] not in current_elf_positions) and
+                        (next_pos_set[2] not in current_elf_positions)):  
+                            elf['next_pos'] = next_pos_set[0]
+                            elf['last_dir'] = d
+                            elves[e] = elf
+                            proposed = True
+                            break
+            if printing:
+                print(r+1,elf,list('nswe')[elf['last_dir']%4])
+            
+        # evaluate proposed positions
+        proposed_elf_positions =  [elf['next_pos'] for elf in elves]
+
+        for e in range(0,len(elves)):
+            elf = dict(elves[e])
+            # dups
+            if proposed_elf_positions.count(elf['next_pos']) > 1:
+                elf['next_pos'] = elf['pos']
+            else:
+                elf['pos'] = elf['next_pos']            
+            elves[e] = elf
+
+        xs = []
+        ys = []
+        
+        for e in range(0,len(elves)):
+            elf = dict(elves[e])
+            xs.append(elf['pos'][0])
+            ys.append(elf['pos'][1])
+
+        if printing:   
+            print()                   
+            grid = []
+            for y in range(-max(ys),max(ys)+1):
+                row = []
+                for x in range(-max(xs),max(xs)+1):
+                    row.append('.')
+                grid.append(row)
+            
+            for elf in elves:
+                [x,y] = dict(elf)['pos']
+                grid[y][x] = '#'#str(elf['elf_no'])
+            
+            grid[0][0] = '0'
+            for row in grid:
+                rowstr = ''
+                for r in row:
+                    rowstr += r
+                print(rowstr)
+        r += 1
+    
+    return r
 
 def main():
     """
@@ -113,16 +226,16 @@ def main():
     testfile  = "tests.txt"
     inputfile = "inputs.txt"
 
-    print("Advent-Of-Code 2022 - Day18")
+    print("Advent-Of-Code 2022 - Day23\n")
 
-    print(f'Tests : Answer to Part 1 = {Part1(testfile, printing = True)}')    
-    # print(f'Tests : Answer to Part 2 = {Part2(testfile, printing = False)}\n')
+    print(f'Tests : Answer to Part 1 = {Part1(testfile, printing = False)}')    
+    print(f'Tests : Answer to Part 2 = {Part2(testfile, printing = False)}\n')
 
-    # assert(Part1(testfile) == 3)
-    # assert(Part2(testfile) == 1623178306)
+    assert(Part1(testfile) == 110)
+    assert(Part2(testfile) == 20)
 
-    # print(f'Inputs: Answer to Part 1 = {Part1(inputfile, printing = False)}')
-    # print(f'Inputs: Answer to Part 2 = {Part2(inputfile, printing = False)}')
+    print(f'Inputs: Answer to Part 1 = {Part1(inputfile, printing = False)}')
+    print(f'Inputs: Answer to Part 2 = {Part2(inputfile, printing = False)}')
 
 if __name__ == "__main__":
     main()
