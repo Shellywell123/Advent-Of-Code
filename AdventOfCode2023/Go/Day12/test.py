38a555dc9e7623aabf61e716491041bd0e0b0132
Shellywell123 <benjamin@shellswell.com>
2024-01-10 17:56:49 +0000

    AOC 2023 Day12 Go

---
+++ AdventOfCode2023/Go/Day12/test.py
@@ -0,0 +1,35 @@
+def count(cfg, nums):
+    if cfg == "":
+        return 1 if nums == () else 0
+
+    if nums == ():
+        return 0 if "#" in cfg else 1
+
+    result = 0
+    
+    if cfg[0] in ".?":
+        result += count(cfg[1:], nums)
+        
+    if cfg[0] in "#?":
+        if nums[0] <= len(cfg) and "." not in cfg[:nums[0]] and (nums[0] == len(cfg) or cfg[nums[0]] != "#"):
+            result += count(cfg[nums[0] + 1:], nums[1:])
+
+    return result
+
+total = 0
+
+for line in open("tests.txt"):
+    cfg, nums = line.split()
+    nums = tuple(map(int, nums.split(",")))
+    total += count(cfg, nums)
+
+print(total)
+
+total = 0
+
+for line in open("inputs.txt"):
+    cfg, nums = line.split()
+    nums = tuple(map(int, nums.split(",")))
+    total += count(cfg, nums)
+
+print(total)
